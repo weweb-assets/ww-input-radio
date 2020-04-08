36b340bb465cb27c19d63a1dfd332e397366cfc4
Marc Fabre <nds.kram@gmail.com>
2020-04-08 10:34:40 +0200

    New config with ww-config.json

--- src/wwObject.vue
+++ src/wwObject.vue
@@ -13,67 +13,67 @@
 
 <script>
 /* wwManager:start */
-import wwRadioPopupConfig from './popup/wwRadioPopupConfig.vue'
+import wwRadioPopupConfig from "./popup/wwRadioPopupConfig.vue";
 
-wwLib.wwPopups.addPopup('wwRadioPopupConfig', wwRadioPopupConfig)
+wwLib.wwPopups.addPopup("wwRadioPopupConfig", wwRadioPopupConfig);
 
-wwLib.wwPopups.addStory('WWFORM_RADIO_CONFIG', {
+wwLib.wwPopups.addStory("WWFORM_RADIO_CONFIG", {
     title: {
-        en: 'Radio Configuration',
-        fr: 'Configuration du bouton radio'
+        en: "Radio Configuration",
+        fr: "Configuration du bouton radio"
     },
-    type: 'wwRadioPopupConfig',
+    type: "wwRadioPopupConfig",
     buttons: {
         OK: {
             text: {
-                en: 'Ok',
-                fr: 'Valider'
+                en: "Ok",
+                fr: "Valider"
             },
             next: false
         }
     }
-})
+});
 
-wwLib.wwPopups.addStory('WWFORM_RADIO_OPTIONS', {
+wwLib.wwPopups.addStory("WWFORM_RADIO_OPTIONS", {
     title: {
-        en: 'Edit Radio',
-        fr: 'Editer le bouton radio'
+        en: "Edit Radio",
+        fr: "Editer le bouton radio"
     },
-    type: 'wwPopupEditWwObject',
+    type: "wwPopupEditWwObject",
     buttons: null,
     storyData: {
         list: {
             RADIO_CONFIG: {
                 separator: {
-                    en: 'Configuration',
-                    fr: 'Configuration'
+                    en: "Configuration",
+                    fr: "Configuration"
                 },
                 title: {
-                    en: 'Change radio configuration',
-                    fr: 'Changer la configuration de bouton radio'
+                    en: "Change radio configuration",
+                    fr: "Changer la configuration de bouton radio"
                 },
                 desc: {
-                    en: '',
-                    fr: ''
+                    en: "",
+                    fr: ""
                 },
-                icon: 'wwi wwi-config',
-                shortcut: 'c',
-                next: 'WWFORM_RADIO_CONFIG'
+                icon: "wwi wwi-config",
+                shortcut: "c",
+                next: "WWFORM_RADIO_CONFIG"
             }
         }
     }
-})
+});
 /* wwManager:end */
 
 export default {
     name: "ww-form-radio",
     props: {
-        wwObjectCtrl: Object,
+        wwObjectCtrl: Object
     },
     data() {
         return {
             wwLang: wwLib.wwLang
-        }
+        };
     },
     computed: {
         wwObject() {
@@ -81,16 +81,37 @@ export default {
         }
     },
     methods: {
+        init() {
+            this.wwObject.content.data = this.wwObject.content.data || {};
+            this.wwObject.content.data.config = this.wwObject.content.data.config || {};
+
+            if (this.wwObject.content.data.isNew) {
+                this.wwObject.content.data.config.options = [
+                    {
+                        value: "value",
+                        wwObject: wwLib.wwObject.getDefault({ type: "ww-text" })
+                    },
+                    {
+                        value: "value2",
+                        wwObject: wwLib.wwObject.getDefault({ type: "ww-text" })
+                    }
+                ];
+
+                delete this.wwObject.content.data.isNew;
+            }
+
+            this.wwObjectCtrl.update(this.wwObject);
+        },
         /* wwManager:start */
         async options() {
-            let copyObj = JSON.parse(JSON.stringify(this.wwObject)) // to clean
-            copyObj.uniqueId += 1
+            let copyObj = JSON.parse(JSON.stringify(this.wwObject)); // to clean
+            copyObj.uniqueId += 1;
             let options = {
-                firstPage: 'WWFORM_RADIO_OPTIONS',
+                firstPage: "WWFORM_RADIO_OPTIONS",
                 data: {
-                    wwObject: copyObj,
+                    wwObject: copyObj
                 }
-            }
+            };
             try {
                 const result = await wwLib.wwPopups.open(options);
                 wwLib.wwObjectHover.setLock(this);
@@ -98,31 +119,29 @@ export default {
                     RADIO CONFIG
                 \================================================================================================*/
                 this.wwObject.content.data.config = this.wwObject.content.data.config || {};
-                if (typeof (result.radioConfig) != 'undefined') {
-                    if (typeof (result.radioConfig.required) != 'undefined') {
+                if (typeof result.radioConfig != "undefined") {
+                    if (typeof result.radioConfig.required != "undefined") {
                         this.wwObject.content.data.config.required = result.radioConfig.required;
                     }
-                    if (typeof (result.radioConfig.name) != 'undefined') {
+                    if (typeof result.radioConfig.name != "undefined") {
                         this.wwObject.content.data.config.name = result.radioConfig.name;
                     }
-                    if (typeof (result.radioConfig.options) != 'undefined') {
+                    if (typeof result.radioConfig.options != "undefined") {
                         this.wwObject.content.data.config.options = result.radioConfig.options;
                     }
                     this.wwObjectCtrl.update(this.wwObject);
                 }
             } catch (err) {
-                wwLib.wwLog.error('ERROR', err)
+                wwLib.wwLog.error("ERROR", err);
             }
             wwLib.wwObjectHover.removeLock();
         }
         /* wwManager:end */
     },
     mounted() {
-        this.wwObject.content.data = this.wwObject.content.data || {}
-        this.wwObject.content.data.config = this.wwObject.content.data.config || {}
+        this.init();
 
-        this.wwObjectCtrl.update(this.wwObject)
-        this.$emit('ww-loaded', this);
+        this.$emit("ww-loaded", this);
     }
 };
 </script>
