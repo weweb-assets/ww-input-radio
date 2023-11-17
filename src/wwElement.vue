3896a2bb2e64766e2d2cbc0c5a8bef8163798912
Marc Fabre <nds.kram@gmail.com>
2023-11-17 12:20:35 +0100

    Corrected striping

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -30,10 +30,12 @@ export default {
         /* wwEditor:end */
     },
     emits: ['update:content:effect'],
+    /* wwEditor:start */
     setup() {
         const { createElement } = wwLib.useCreateElement();
         return { createElement };
     },
+    /* wwEditor:end */
     data() {
         return {
             internalValue: null,
