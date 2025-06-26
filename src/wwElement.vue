1ebd61bccc78cfd55342c54e86a96e4b061fcfb0
Léo Coletta <mail.leo.coletta@gmail.com>
2025-06-26 18:49:09 +0200

    refactor(ww-input-radio): use native change event instead of v-model
    - Removed v-model in favor of native change event
    - Removed editor click overlay (no longer needed)
    - Prevent default on change event when in editing mode
    - Cleaner implementation without overlay element

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -3,7 +3,6 @@
         <input
             class="ww-input-radio"
             ref="inputRef"
-            v-model="isInternalChecked"
             :id="inputId"
             :style="$attrs.style"
             :class="{ 'no-appearance': content.appearance === 'custom' }"
@@ -12,13 +11,11 @@
             :required="isRequired"
             :checked="isChecked"
             :disabled="isDisabled"
-            :readonly="isReadonly || isEditing"
+            :readonly="isReadonly"
             ww-responsive="radio-input"
             type="radio"
-            @click.stop
+            @change="handleChange"
         />
-        <!-- Editor click overlay -->
-        <div v-if="isEditing" class="ww-editor-click-overlay"></div>
     </div>
 </template>
 
@@ -38,21 +35,21 @@ export default {
     emits: ['add-state', 'remove-state', 'update:sidepanel-content'],
     setup(props, { emit }) {
         const inputRef = ref(null);
-        
+
         // Generate unique ID for the radio input
         const generatedId = `ww-input-radio-${useId()}`;
-        
+
         // Use custom ID if set, otherwise use generated ID
         const inputId = computed(() => props.wwElementState.props?.attributes?.id || generatedId);
-        
+
         // Register with parent label if available
         const useLabelChild = inject('_wwLabel:useLabelChild', null);
-        
+
         // Create computed name for label - should show radio value
         const radioLabelName = computed(() => {
             return props.content.value || 'Radio option';
         });
-        
+
         // Register with label if inside one
         if (useLabelChild) {
             useLabelChild(props.wwElementState.uid, radioLabelName);
@@ -69,30 +66,20 @@ export default {
             insideRadioGroup,
         } = useWewebRadio(props, emit);
 
-        return { 
+        return {
             inputRef,
             inputId,
-            isChecked, 
-            name, 
-            value, 
-            select, 
-            composableReadonly, 
-            isDisabled, 
+            isChecked,
+            name,
+            value,
+            select,
+            composableReadonly,
+            isDisabled,
             isRequired,
             insideRadioGroup,
         };
     },
     computed: {
-        isInternalChecked: {
-            get() {
-                return this.isChecked;
-            },
-            set(value) {
-                if (value && !this.isEditing) {
-                    this.select();
-                }
-            },
-        },
         isEditing() {
             /* wwEditor:start */
             return this.wwEditorState.isEditing;
@@ -136,6 +123,17 @@ export default {
         },
     },
     methods: {
+        handleChange(event) {
+            // Prevent default and stop the change in editing mode
+            if (this.isEditing) {
+                event.preventDefault();
+                return;
+            }
+            // For normal operation, trigger select
+            if (event.target.checked) {
+                this.select();
+            }
+        },
         handleWrapperClick() {
             // Allow clicking on the wrapper to select the radio
             if (!this.isEditing && !this.isReadonly) {
@@ -151,7 +149,7 @@ export default {
     position: relative;
     display: inline-block;
     cursor: pointer;
-    
+
     &:has(input[readonly]),
     &:has(input[disabled]) {
         cursor: not-allowed;
@@ -160,27 +158,14 @@ export default {
 
 .ww-input-radio {
     cursor: inherit;
-    
+
     &:focus-visible {
         outline: none;
     }
-    
+
     &.no-appearance {
         appearance: none;
     }
 }
 
-/* wwEditor:start */
-.ww-editor-click-overlay {
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    z-index: 1;
-    cursor: pointer;
-    // Transparent but clickable
-    background: transparent;
-}
-/* wwEditor:end */
 </style>
